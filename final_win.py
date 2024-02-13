18d0f7a83c6eaeaff7afda8cf828a31a542fd579
mywebnik <131589705+mywebnik@users.noreply.github.com>
2024-02-13 17:21:19 +0300

    Update final_win.py
    3

--- final_win.py
+++ final_win.py
@@ -1,1 +1,38 @@
 # напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+
+from instr import *  #загружаем переменные из файла instr.py
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear() # устанавливает, как будет выглядеть окно
+        self.initUI() # создаём и настраиваем графические элементы
+        self.show()  # показываем окно
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.layout = QVBoxLayout()
+        self.index_text = QLabel(txt_index)
+        self.workheart_text = QLabel(txt_workheart)
+        self.layout.addWidget(self.index_text)
+        self.layout.addWidget(self.workheart_text)
+        self.setLayout(self.layout)
+    def initUI(self):
+        ''' создает графические элементы '''
+        self.workh_text = QLabel(txt_workheart + self.results())
+        self.index_text = QLabel(txt_index + str(self.index))
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout_line)
+
+    ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
